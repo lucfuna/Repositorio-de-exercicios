3019565caa30c5edca12fcf004289bbcc246afc2
lucfuna <lucfuna@hotmail.com>
2022-10-27 09:12:05 -0300

    salvando primeiro exercicio em js

---
+++ Modulo_Fundamentos/Introducao_JavaScript/Dia1_Primeios_Passos_JS/scripts.js
@@ -0,0 +1,11 @@
+//criando uma variavel e atribuindo um valor
+const myName = "Lucas";
+const birthCity = "Serra";
+let birthYear = 1993;
+
+
+//mostrando resultado no console
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
