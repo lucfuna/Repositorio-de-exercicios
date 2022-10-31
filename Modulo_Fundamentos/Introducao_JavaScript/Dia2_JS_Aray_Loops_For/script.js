809487cfdf024930507371a0a581946010cd779b
lucfuna <lucfuna@hotmail.com>
2022-10-31 10:27:21 -0300

    iniciando estudos em array e For

---
+++ Modulo_Fundamentos/Introducao_JavaScript/Dia2_JS_Aray_Loops_For/script.js
@@ -0,0 +1,15 @@
+//Variavel simples
+let pizza1 = '4 Queijos';
+let pizza2 = 'Frango com Catupiry';
+let pizza3 = 'Marguerita';
+let pizza4 = 'Palmito';
+let pizza5 = 'Chocolate';
+// Caso eu queira logar todas essas variavel teria que dar um log em cada uma delas
+
+
+//declarando um array!!!!
+
+let pizzas = ['4 Queijos','Frango com Catupiry', 'Marguerita', 'Palmito', 'Chocolate'];
+// lembrar que o posicionamento dentro do array começa com 0!!
+
+console.log(pizzas);
