179da84fe1192fd3129ae3a731eba895bad98d49
lucfuna <lucfuna@hotmail.com>
2022-10-31 11:01:52 -0300

    atualizando

--- Modulo_Fundamentos/Introducao_JavaScript/Dia2_JS_Aray_Loops_For/FOR.js
+++ Modulo_Fundamentos/Introducao_JavaScript/Dia2_JS_Aray_Loops_For/FOR.js
@@ -1,7 +1,7 @@
 //Estrutura do for!!
 //For ('variavel que vai contar as repetições'; 'define a quantidade de repetições'; 'como vai ser a atuaização apos cada repetição';)
 var numero = 7;
-for (var contador = 1; contador <= 9; contador++) {
+for (var contador = 1; contador <= 9; contador+= 1) {
     console.log(numero * contador);
 }
 
@@ -9,7 +9,7 @@ for (var contador = 1; contador <= 9; contador++) {
 //Exercicio 2 da aula de for!!
 var listaDeNomes = ["Joana", "Maria", "Lucas"];
 
-for ( var indice = 0; indice < listaDeNomes.length; indice++){
+for ( var indice = 0; indice < listaDeNomes.length; indice+= 1){
     var mensagem = "Boas Vindas, " + listaDeNomes[indice] + "!";
     console.log(mensagem);
 }
