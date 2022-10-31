113dee56802dd64117f04e0ff3be216e39362657
lucfuna <lucfuna@hotmail.com>
2022-10-31 13:20:20 -0300

    Aplicando os aprendizados no exercicio final

---
+++ Modulo_Fundamentos/Introducao_JavaScript/Dia3_JS_Lógica_de_Programação_Algoritmos/Exercici0_Fixação_Final.js
@@ -0,0 +1,93 @@
+//Exercicio de fixação do final do modulo!!!
+
+//ITEM 1 - crie um algoritmo que imprima na tela o fatorial de 10.
+console.log("ITEM 1 - Crie um algoritmo que imprima na tela o fatorial de 10.")
+console.log("")
+
+let fatorial = 1;
+
+for (let index = 10; index > 0; index -= 1) {
+  fatorial *= index;
+}
+
+console.log(fatorial);
+console.log("")
+console.log("")
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//ITEM 2 - Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. Por exemplo, a palavra “banana” seria invertida para “ananab”. Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+console.log("ITEM 2 -Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. Por exemplo, a palavra “banana” seria invertida para “ananab”. Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.")
+console.log("")
+
+let word = 'tryber';
+let reverseWord = '';
+
+for (let index = 0; index < word.length; index += 1) {
+  reverseWord += word[word.length - 1 - index];
+}
+
+console.log(reverseWord);
+console.log("")
+
+//Outra solução possivel para invertar a palavra!!!!!
+let word1 = 'tryber';
+let reverseWord1 = '';
+
+reverseWord1 = word1.split('').reverse().join('');
+
+console.log(reverseWord1);
+
+console.log("")
+console.log("")
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//ITEM 3 - Utilize a estrutura de repetição for para escrever dois algoritmos: um que imprima no console a maior palavra desse array e outro que imprima a menor. Considere o número de caracteres de cada palavra.
+console.log("ITEM 3 -Utilize a estrutura de repetição for para escrever dois algoritmos: um que imprima no console a maior palavra desse array e outro que imprima a menor. Considere o número de caracteres de cada palavra.")
+console.log("")
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let biggestWord = array[0];
+let smallestWord = array[0];
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length > biggestWord.length) {
+    biggestWord = array[index];
+  }
+}
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length < smallestWord.length) {
+    smallestWord = array[index];
+  }
+}
+
+console.log(biggestWord);
+console.log(smallestWord);
+
+console.log("")
+console.log("")
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//ITEM 4 - Um número primo é um número inteiro maior do que 1 que possui somente dois divisores, ou seja, é divisível por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que imprima no console o maior número primo entre 2 e 50.
+console.log("ITEM 4 -Um número primo é um número inteiro maior do que 1 que possui somente dois divisores, ou seja, é divisível por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que imprima no console o maior número primo entre 2 e 50.")
+console.log("")
+
+let biggestPrimeNumber = 0;
+
+for (let currentNumber = 2; currentNumber <= 50; currentNumber += 1) {
+  let isPrime = true;
+  for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+    if (currentNumber % currentDivisor === 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    biggestPrimeNumber = currentNumber;
+  }
+}
+
+console.log(biggestPrimeNumber);
+
+
