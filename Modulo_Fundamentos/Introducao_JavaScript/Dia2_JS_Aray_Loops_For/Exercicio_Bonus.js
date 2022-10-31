7aa14645a2399347895d120dc0a2f6b0a21c4358
lucfuna <lucfuna@hotmail.com>
2022-10-31 12:07:03 -0300

    Verificando o exercicio bonus do final da aula

---
+++ Modulo_Fundamentos/Introducao_JavaScript/Dia2_JS_Aray_Loops_For/Exercicio_Bonus.js
@@ -0,0 +1,54 @@
+//Item 1 - Ordene o array numbers em ordem crescente e imprima seus valores;
+console.log("ITEM 1 - Ordene o array numbers em ordem crescente e imprima seus valores;")
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 1; index < numbers.length; index += 1) {
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+      if (numbers[index] < numbers[secondIndex]) {
+        let position = numbers[index];
+        numbers[index] = numbers[secondIndex];
+        numbers[secondIndex] = position;
+      }
+    }
+}
+  
+  console.log(numbers);
+  //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  //Item 2 - Ordene o array numbers em ordem decrescente e imprima seus valores;
+console.log("ITEM 2 - Ordene o array numbers em ordem decrescente e imprima seus valores;")
+
+for (let index = 1; index < numbers.length; index += 1) {
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+      if (numbers[index] > numbers[secondIndex]) {
+        let position = numbers[index];
+        numbers[index] = numbers[secondIndex];
+        numbers[secondIndex] = position;
+      }
+    }
+}
+  
+  console.log(numbers);
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  //Item 3 - Agora, crie um novo array a partir do array numbers, sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push. O resultado deve ser o array abaixo:
+console.log("ITEM 3 - Agora, crie um novo array a partir do array numbers, sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push. O resultado deve ser o array abaixo:")
+// [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54]
+let numbers1 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let newArray = [];
+
+for (let index = 0; index < numbers1.length; index += 1) {
+  if (index + 1 < numbers1.length) {
+    newArray.push(numbers1[index] * numbers1[index + 1]);
+  } else {
+    newArray.push(numbers1[index] * 2);
+  }
+}
+
+console.log(newArray);
+
+
+
+
+
