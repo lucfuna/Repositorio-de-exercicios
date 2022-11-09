c7f7c2e506832e7adc486f0e089958c005946cdb
lucfuna <lucfuna@hotmail.com>
2022-11-09 12:05:07 -0300

    aprendizados sobre Template Literals

---
+++ Modulo_Fundamentos/Introducao_JavaScript/Dia5_JS_ES6/Aprendendo_Template_Literals.js
@@ -0,0 +1,29 @@
+//Como era formado frazem em strings sem o Template Literals
+
+const myName = 'Isabella';
+console.log('Hello' + ' ' + myName + '!');
+
+
+//Agora com a utilização de Template Literals
+
+const myName2 = 'Isabella';
+console.log(`Welcome ${myName2}!`);
+
+console.log('')
+console.log('-----------------------------------------------')
+
+//Como era feito para quebra de linha antigamente!!!
+// Sem o template literals:
+console.log('Primeira linha;\n' + 'Segunda linha;\n' + 'Terceira linha;\n');
+
+//Como era feito para quebra de linha com Template Literals!!!
+// Com o template literals
+console.log(`Primeira linha;
+Segunda linha;
+Terceira linha;`);
+
+console.log('')
+console.log('-----------------------------------------------')
+
+
+
